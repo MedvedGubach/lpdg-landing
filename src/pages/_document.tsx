ced48f93cc4af67b50d43a27b4668aca1533de04
Emmanuel <emmanuel.arana.gutierrez@gmail.com>
2025-08-07 19:26:26 -0600

    small change to accesibility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className="antialiased">
         <Main />
         <NextScript />
